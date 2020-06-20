5c935bf1264cff0bd486da50394005ccc4e14e32
sid-thakur47 <sidthakur258@gmail.com>
2020-06-20 17:08:52 +0530

    ADDED:Automation Script for testing invalid username & password

---
+++ src/test/java/com/bl/selenium/HerokuAppNegativeTest.java
@@ -0,0 +1,87 @@
+package com.bl.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HerokuAppNegativeTest {
+
+    @Test
+    public void userNameTest() {
+
+        //create driver
+        System.setProperty("webdriver.chrome.driver", "C:/Users/Shivani/Desktop/SelinumDemo/src/main/resources/chromedriver.exe");
+        WebDriver webDriver = new ChromeDriver();
+
+        //open test page
+        String url = "http://the-internet.herokuapp.com/login";
+        webDriver.get(url);
+
+        //maximize window
+        webDriver.manage().window().fullscreen();
+
+        //verification
+        String actualUrl = "http://the-internet.herokuapp.com/login";
+        String expectedUrl = webDriver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl, "URL do not match");
+
+        //username
+        WebElement userName = webDriver.findElement(By.id("username"));
+        userName.sendKeys("incorrectusername");
+
+        //password
+        WebElement password = webDriver.findElement(By.id("password"));
+        password.sendKeys("SuperSecretPassword!");
+
+        WebElement loginButton = webDriver.findElement(By.tagName("button"));
+        loginButton.click();
+
+        //verification for incorrect username
+        WebElement errorMessage = webDriver.findElement(By.id("flash"));
+        String expectedMessage = "Your username is invalid! ×";
+        String actualMessage = errorMessage.getText();
+        Assert.assertEquals(expectedMessage, actualMessage);
+        webDriver.quit();
+    }
+
+    @Test
+    public void passwordTest() {
+
+        //create driver
+        System.setProperty("webdriver.chrome.driver", "C:/Users/Shivani/Desktop/SelinumDemo/src/main/resources/chromedriver.exe");
+        WebDriver webDriver = new ChromeDriver();
+
+        //open test page
+        String url = "http://the-internet.herokuapp.com/login";
+        webDriver.get(url);
+
+        //maximize window
+        webDriver.manage().window().fullscreen();
+
+        //verification
+        String actualUrl = "http://the-internet.herokuapp.com/login";
+        String expectedUrl = webDriver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl, "URL do not match");
+
+        //username
+        WebElement userName = webDriver.findElement(By.id("username"));
+        userName.sendKeys("tomsmith");
+
+        //password
+        WebElement password = webDriver.findElement(By.id("password"));
+        password.sendKeys("incorrectpassword");
+
+        WebElement loginButton = webDriver.findElement(By.tagName("button"));
+        loginButton.click();
+
+        //verification for incorrect username
+        WebElement errorMessage = webDriver.findElement(By.id("flash"));
+        String expectedMessage = "Your password is invalid! ×";
+        String actualMessage = errorMessage.getText();
+        Assert.assertEquals(expectedMessage, actualMessage);
+        webDriver.quit();
+    }
+}
--- src/test/java/com/bl/selenium/HerokuAppPositiveTest.java
+++ src/test/java/com/bl/selenium/HerokuAppPositiveTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class HerokuAppTest {
+public class HerokuAppPositiveTest {
 
     @Test
     public void loginTest() {
