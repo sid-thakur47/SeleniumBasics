d8ffe65a865046f6dc8684495942e4a96a1c547d
sid-thakur47 <sidthakur258@gmail.com>
2020-06-20 16:20:41 +0530

    REFACTORED:Changed class name

--- src/test/java/com/bl/selenium/HerokuAppTest.java
+++ src/test/java/com/bl/selenium/HerokuAppTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class PositiveTest {
+public class HerokuAppTest {
 
     @Test
     public void loginTest() {
