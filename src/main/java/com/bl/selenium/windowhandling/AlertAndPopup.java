29168f44da01794002817243fc27c458de0d7d02
sid-thakur47 <sidthakur258@gmail.com>
2020-07-07 15:37:44 +0530

    ADDED:Automation Script for popup handling of Books wagon

--- src/main/java/com/bl/selenium/windowhandling/AlertAndPopup.java
+++ src/main/java/com/bl/selenium/windowhandling/AlertAndPopup.java
@@ -4,6 +4,9 @@ import com.bl.selenium.base.Base;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
 
 import java.util.Iterator;
 import java.util.Set;
@@ -89,6 +92,32 @@ public class AlertAndPopup extends Base {
         element1.click();
         Thread.sleep(5000);
         webDriver.close();
+    }
+    @Test
+    public void booksWagonTest() {
+        try {
+            initializeBase();
+            webDriver.get("https://www.bookswagon.com/login");
+            WebElement userName = webDriver.findElement(By.xpath("//input[@id='ctl00_phBody_SignIn_txtEmail']"));
+            userName.sendKeys(properties.getProperty("email"));
+            WebElement password = webDriver.findElement(By.xpath("//input[@id='ctl00_phBody_SignIn_txtPassword']"));
+            password.sendKeys(properties.getProperty("bpass"));
+            Thread.sleep(5000);
+            webDriver.findElement(By.xpath("//input[@name='ctl00$phBody$SignIn$btnLogin']")).click();
+            Thread.sleep(5000);
+            WebElement cart = webDriver.findElement(By.xpath("//a[@class='cart-pop iframe cart-link sprite cart-linkbg cboxElement']"));
+            cart.click();
+            Thread.sleep(5000);
+            WebElement element = webDriver.findElement(By.xpath("//iframe[@name='cbox1594058220570']"));
+            WebDriverWait wb=new WebDriverWait(webDriver,10);
+            wb.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div//iframe[@name='cbox1594058220570']")));
+            webDriver.switchTo().frame(element);
+            Thread.sleep(5000);
+            webDriver.findElement(By.xpath("//a[@id='BookCart_lvCart_ctrl0_imgDelete']")).click();
+            Thread.sleep(5000);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
